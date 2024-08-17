900f366d9777c39f6da898f18ce8e8479c2ecd84
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-08-17 20:22:47 +0300

    Add PngSavingStrategy

---
+++ Screeny/Screeny.Application/SavingStrategies/PngSavingStrategy.cs
@@ -0,0 +1,24 @@
+﻿using Screeny.Domain.Common;
+using Screeny.Domain.Screenshots;
+using System.Drawing.Imaging;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Screeny.Application.SavingStrategies
+{
+    public sealed class PngSavingStrategy : ISavingStrategy
+    {
+        public void Save(Screenshot screenshot, ScreenshotPath path)
+        {
+            using MemoryStream memoryStream = new([.. screenshot.Image]);
+            using Image image = Image.FromStream(memoryStream);
+
+            image.Save(path, ImageFormat.Png);
+        }
+    }
+}
