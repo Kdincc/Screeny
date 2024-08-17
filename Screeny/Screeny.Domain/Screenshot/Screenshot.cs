544358d5e021da0e4000da81b86bc112ae137755
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-08-17 17:19:07 +0300

    Add Screenshot class to Screenshot folder in Domain

---
+++ Screeny/Screeny.Domain/Screenshot/Screenshot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Screeny.Domain.Screenshot
+{
+    public sealed class Screenshot(string title, byte[] image, Size size, ScreenshotFormat format)
+    {
+        public ScreenshotFormat Format { get; private set; } = format;
+
+        public byte[] Image { get; private set; } = image;
+
+        public string Title { get; private set; } = title;
+
+        public Size Size { get; private set; } = size;
+    }
+}
