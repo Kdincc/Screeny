6e766bd9048c63cf87b56b29dca00b7d76a95544
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-08-17 17:28:48 +0300

    Add ISavingStrategy interface  to Common folder in Domain

---
+++ Screeny/Screeny.Domain/Common/ISavingStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Screeny.Domain.Screenshot;
+
+namespace Screeny.Domain.Common
+{
+    public interface ISavingStrategy
+    {
+        public void Save(Screenshot screenshot, string path);
+    }
+}
