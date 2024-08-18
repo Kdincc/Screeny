165b309e018ea7cd429715cce21e7f201eda90cb
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-08-18 13:38:50 +0300

    Add validation in Create method in ScreenshotImage

--- Screeny/Screeny.Domain/Screenshots/ScreenshotImage.cs
+++ Screeny/Screeny.Domain/Screenshots/ScreenshotImage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Screeny.Domain.Common.Constants;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,62 @@ namespace Screeny.Domain.Screenshots
 
         public static ScreenshotImage FromBytes(byte[] bytes)
         {
+            ThrowIfIsNotCorrectImage(bytes);
+
+            return new ScreenshotImage(bytes);
+        }
+
+        private static void ThrowIfIsNotCorrectImage(byte[] bytes)
+        {
+            if (!IsPng(bytes) && !IsJpeg(bytes) && !IsTiff(bytes))
+            {
+                throw new ArgumentException("The image is not in a correct format.");
+            }
+        }
+
+        private static bool IsPng(byte[] bytes)
+        {
+            if (bytes.Length < ImageSignatures.Png.Length)
+            {
+                return false;
+            }
+
+            if (bytes.Take(ImageSignatures.Png.Length).SequenceEqual(ImageSignatures.Png))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool IsJpeg(byte[] bytes)
+        {
+            if (bytes.Length < ImageSignatures.Jpeg.Length)
+            {
+                return false;
+            }
+
+            if (bytes.Take(ImageSignatures.Jpeg.Length).SequenceEqual(ImageSignatures.Jpeg))
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+        private static bool IsTiff(byte[] bytes)
+        {
+            if (bytes.Length < ImageSignatures.Tiff.Length)
+            {
+                return false;
+            }
+
+            if (bytes.Take(ImageSignatures.Tiff.Length).SequenceEqual(ImageSignatures.Tiff))
+            {
+                return true;
+            }
+
+            return false;
 
         }
     }
