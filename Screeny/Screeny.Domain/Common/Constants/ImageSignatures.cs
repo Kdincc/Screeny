395781cd9033c2122c5277d7313cd988da6ccb6f
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-08-18 13:34:37 +0300

    Add ImageSignatures class to Constants in Domain

---
+++ Screeny/Screeny.Domain/Common/Constants/ImageSignatures.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Screeny.Domain.Common.Constants
+{
+    public static class ImageSignatures
+    {
+        public static readonly byte[] Png = [0x89, 0x50, 0x4E, 0x47];
+        public static readonly byte[] Jpeg = [0xFF, 0xD8, 0xFF];
+        public static readonly byte[] Tiff = [0x49, 0x49, 0x2A, 0x00];
+    }
+}
