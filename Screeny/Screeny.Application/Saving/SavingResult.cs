5e564bc3f218c3bc8485d34c63d44a6c60419fed
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-08-18 13:55:15 +0300

    Add SavingResult class in Saving

---
+++ Screeny/Screeny.Application/Saving/SavingResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Screeny.Application.Saving
+{
+    public sealed class SavingResult
+    {
+        public bool Success { get; }
+        public string Message { get; }
+
+        public SavingResult(bool success, string message = "Success")
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
