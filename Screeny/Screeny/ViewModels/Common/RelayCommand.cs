f6c5357891b32e08c2fbab36ac738565df582ef5
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-08-21 19:31:36 +0300

    Add ViewModelBase to ViewModels/Common folder in Screeny project

---
+++ Screeny/Screeny/ViewModels/Common/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Screeny.ViewModels.Common
+{
+    public sealed class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute)
+        {
+            _execute = execute;
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute is null)
+            {
+                return true;
+            }
+
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
